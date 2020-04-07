5bbe41a67cc7ac5a01b920a35e8914b3aa2aea08
Shawn Giroux <shawn.giroux90@gmail.com>
2020-04-07 13:58:35 -0400

    Adding color to titles

--- src/display.rs
+++ src/display.rs
@@ -1,3 +1,5 @@
+use colored::*;
+
 pub fn clear_screen() {
     print!("\x1B[2J");
 }
@@ -6,6 +8,6 @@ pub fn draw_stories(stories: Vec<crate::hacker_news::Story>) {
     for story in stories {
         let title = story.title;
         let url = story.url.unwrap();
-        println!("{}\n{:?}\n\n", title, url);
+        println!("{}\n{:?}\n\n", title.yellow().bold(), url);
     }
 }
