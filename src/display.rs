436a75c624e37a4d6c18bae135372dceea64fe53
Shawn Giroux <shawn.giroux90@gmail.com>
2020-04-07 13:59:03 -0400

    Handling empty urls

--- src/display.rs
+++ src/display.rs
@@ -7,7 +7,11 @@ pub fn clear_screen() {
 pub fn draw_stories(stories: Vec<crate::hacker_news::Story>) {
     for story in stories {
         let title = story.title;
-        let url = story.url.unwrap();
+        let url = match story.url {
+            Some(x) => x,
+            None => "No URL Provided".to_string(),
+        };
+
         println!("{}\n{:?}\n\n", title.yellow().bold(), url);
     }
 }
